ea3b36470694c3bd8919a9960c65a53f8e598c2f
keyvanmhr <87208291+kevinmhr@users.noreply.github.com>
2024-06-05 19:03:27 +0000

    Add files via upload

---
+++ map.c
@@ -0,0 +1,220 @@
+ 
+
+#include <allegro.h>
+#include <math.h>
+char *bitmapbuffer;
+int vertices[]={50,50,70,100,150,50};
+
+int orient[]={10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,160,150,140,130,120,110,100,90,80,70,60,50,40,30,20,10};
+int z;
+int y;
+int k;
+int x;
+int x2;
+int k;
+int ti;
+int ti2;
+int ti3;
+int ti4;
+int swi=1;
+int swi2=0;
+int f;
+int shift;
+int incdec=1;
+
+
+int map[]={
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int timer(){
+
+
+
+
+     if(ti>1000){z=z+1;}
+          if(ti<1000){z=z-1;}
+ if (ti>=2000){incdec=-1;}
+   if (ti<=0){incdec=1;}
+  if (shift>=32){shift=1;}
+
+
+             ti=ti+(incdec);
+
+          //x+=y/10;
+
+
+
+
+         x+=ti/500+(z)/20;
+         x2+=ti/500+(z)/20;
+         x2=x2-1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+int drawroad (){
+
+
+                          ti2++;
+    if (ti2>100){timer();ti2=0;}
+
+
+              k=k+1;
+             // drawroad();
+
+          if (k>=400){k=0;  x=0;x2=0;}
+
+
+
+
+      //   for (int k=100;k<200;k=k+2)  {
+          hline(screen,((x)+68)*2,((-k+800)/4),(x2+122)*2,3);
+          hline(screen,((x)+60)*2,((-k+800)/4),(x2+120)*2,1);
+
+
+
+
+
+
+
+}
+
+
+
+
+int bg(){
+
+
+
+              f=f+1;
+
+              y=y+2;
+
+             // drawroad();
+           if (y>=200){y=0;}
+          if (f>=100){f=0;}
+
+             hline(screen,0,f+100,320,9);
+
+        hline(screen,0,y-100,320,7);
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+int main(int argc, char *argv[])
+{
+   BITMAP *the_image;
+   BITMAP *the_block1;
+   PALETTE the_palette;
+   BITMAP *surface;
+   BITMAP *sprites;
+   int x=1;
+   int *dots;
+   if (allegro_init() != 0)
+      return 1;
+
+  // if (argc != 2) {
+    //  allegro_message("Usage: 'exbitmap filename.[bmp|lbm|pcx|tga]'\n");
+    //  return 1;
+  // }
+
+   install_keyboard();
+
+   if (set_gfx_mode(GFX_AUTODETECT, 320, 200, 0, 0) != 0) {
+      if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0) {
+       set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+         allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
+ return 1;
+      }
+   }
+
+bitmapbuffer="metal.bmp";
+   /* read in the bitmap file */
+
+   the_image= load_bitmap(bitmapbuffer, the_palette);
+ 
+   set_palette(the_palette);
+     int i;
+     int k;
+     int j;
+     j=0;
+       the_block1= load_bitmap("block1.bmp",the_palette);
+           k=1;
+           for (k=0;k<600;k++){
+ 
+
+            if (i>32){j=j+10;i=0;}
+           i++;
+        if (map[k]==1) {
+
+
+       draw_sprite(screen,the_image, i*10,j); }
+       if (map[k]==2) {
+
+
+       draw_sprite(screen,the_block1, i*10,j); }
+
+
+                                   }
+  //
+     while (i!=0){
+ 
+
+
+    }
+   destroy_bitmap(the_image);
+                             destroy_bitmap(screen);
+
+
+
+   
+   readkey();
+//   return 0;
+}
+
+END_OF_MAIN()
