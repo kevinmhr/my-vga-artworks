c9711a806b3e20ca6b5df81ed953b06abb6ab972
keyvanmhr <87208291+kevinmhr@users.noreply.github.com>
2025-06-10 08:38:17 -0400

    Add files via upload

--- bitmap.c
+++ bitmap.c
@@ -1,200 +1,282 @@
-/*
- *    Example program for the Allegro library, by Shawn Hargreaves.
- *
- *    This program demonstrates how to load and display a bitmap
- *    file.  You have to use this example from the command line to
- *    specify as first parameter a graphic file in one of Allegro's
- *    supported formats.  If the file is loaded successfully,
- *    it will be displayed until you press a key.
- */
-
-
-#include <allegro.h>
-#include <math.h>
-char *bitmapbuffer;
-int vertices[]={50,50,70,100,150,50};
-
-int orient[]={10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,160,150,140,130,120,110,100,90,80,70,60,50,40,30,20,10};
-int y;
-int k;
-int x;
-int k=100;
-int ti;
-int ti2;
-int ti3;
-int ti4;
-int swi=1;
-int swi2=0;
-int f;
-int shift;
-int incdec=1;
-int bg(){
-
-
-
-             for (int k=100;k<200;k=k+2)  {
-
-    hline(screen,0,k,320,9);
-
-        hline(screen,0,(k)-100,320,7);
-                  }//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-int timer(){
-
- if (ti<=0){incdec=1;}
- if (ti>=2000){incdec=-1;}
- 
-  ti=ti+(incdec);
-
-         x+=(ti/200)+k/100;
-
-
-ti3++;
-
-if (y>=200){y=0;  }
-if (ti3>200){y++;shift++; ti3=0;bg();}
-  if (shift>=32){shift=1;}
-
-
-
-
-          //x+=y/10;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-int drawroad (){
-
-
-
-
-              k=k+1;
-             // drawroad();
-
-          if (k>=400){k=0;x=0;}
-
-
-
-            ti2++;
-     if (ti2>=5){ti2=0;timer();  }
-
-      //   for (int k=100;k<200;k=k+2)  {
-
-          hline(screen,-(x)+140,(k/4)+100,x+160,1);
-
-
-
-}
-
-
-int main(int argc, char *argv[])
+/**************************************************************************
+ * bitmap.c                                                               *
+ * written by David Brackeen                                              *
+ * http://www.brackeen.com/home/vga/                                      *
+ *                                                                        *
+ * This is a 16-bit program.                                              *
+ * Tab stops are set to 2.                                                *
+ * Remember to compile in the LARGE memory model!                         *
+ * To compile in Borland C: bcc -ml bitmap.c                              *
+ *                                                                        *
+ * This program will only work on DOS- or Windows-based systems with a    *
+ * VGA, SuperVGA or compatible video adapter.                             *
+ *                                                                        *
+ * Please feel free to copy this source code.                             *
+ *                                                                        *
+ * DESCRIPTION: This program demostrates drawing bitmaps, including       *
+ * transparent bitmaps.                                                   *
+ **************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <mem.h>
+#include <conio.h>
+
+#define VIDEO_INT           0x10      /* the BIOS video interrupt. */
+#define SET_MODE            0x00      /* BIOS func to set the video mode. */
+#define VGA_256_COLOR_MODE  0x13      /* use to set 256-color mode. */
+#define TEXT_MODE           0x03      /* use to set 80x25 text mode. */
+
+#define SCREEN_WIDTH        320       /* width in pixels of mode 0x13 */
+#define SCREEN_HEIGHT       200       /* height in pixels of mode 0x13 */
+#define NUM_COLORS          256       /* number of colors in mode 0x13 */
+
+typedef unsigned char  byte;
+typedef unsigned short word;
+typedef unsigned long  dword;
+
+byte *VGA=(byte *)0xA0000000L;        /* this points to video memory. */
+word *my_clock=(word *)0x0000046C;    /* this points to the 18.2hz system
+					 clock. */
+char *buffer;
+typedef struct tagBITMAP              /* the structure for a bitmap. */
 {
-   BITMAP *the_image;
-   PALETTE the_palette;
-   BITMAP *surface;
-   int x=1;
-   int *dots;
-   if (allegro_init() != 0)
-      return 1;
+  word width;
+  word height;
+  byte *data;
+} BITMAP;
 
-  // if (argc != 2) {
-    //  allegro_message("Usage: 'exbitmap filename.[bmp|lbm|pcx|tga]'\n");
-    //  return 1;
-  // }
+/**************************************************************************
+ *  fskip                                                                 *
+ *     Skips bytes in a file.                                             *
+ **************************************************************************/
 
-   install_keyboard();
-
-   if (set_gfx_mode(GFX_AUTODETECT, 320, 200, 0, 0) != 0) {
-      if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0) {
-	 set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
-	 allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
-	 return 1;
-      }
-   }
-  bitmapbuffer="planet.pcx";
-   /* read in the bitmap file */
-   the_image = load_pcx(bitmapbuffer, the_palette);
-   if (!the_image) {
-      set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
-      allegro_message("Error reading bitmap file '%s'\n", bitmapbuffer);
-      return 1;
-   }
-
-   /* select the bitmap palette */
-  // set_palette(the_palette);
-     int i;
-     i=1;
-            blit(the_image, screen, 0, 0, 150,
-50, the_image->w, the_image->h);
-         //   int x;
-
-             for (int k=100;k<200;k=k+2)  {
-
-    hline(screen,0,k,320,9);
-
-        hline(screen,0,k-100,320,7);
-                  }//}
-
-
-
-     while (i!=0){
-   /* blit the image onto the screen */
-
-     //timer();
-
-
-      
-      drawroad();
-
-        //          }//}
-        //     for (int i=0;i<5;i++){
-
-
-
-
-
-      polygon(screen,3,vertices,3);//}
-
-
-    }
-   destroy_bitmap(the_image);
-                             destroy_bitmap(screen);
-
-
-
-   
-   readkey();
-//   return 0;
+void fskip(FILE *fp, int num_bytes)
+{
+   int i;
+   for (i=0; i<num_bytes; i++)
+      fgetc(fp);
 }
 
-END_OF_MAIN()
+/**************************************************************************
+ *  set_mode                                                              *
+ *     Sets the video mode.                                               *
+ **************************************************************************/
+
+void set_mode(byte mode)
+{
+  union REGS regs;
+
+  regs.h.ah = SET_MODE;
+  regs.h.al = mode;
+  int86(VIDEO_INT, &regs, &regs);
+}
+
+/**************************************************************************
+ *  load_bmp                                                              *
+ *    Loads a bitmap file into memory.                                    *
+ **************************************************************************/
+
+void load_bmp(char *file,BITMAP *b)
+{
+  FILE *fp;
+  long index;
+  word num_colors;
+  int x;
+
+  /* open the file */
+  if ((fp = fopen(file,"rb")) == NULL)
+  {
+    printf("Error opening file %s.\n",file);
+    exit(1);
+  }
+
+  /* check to see if it is a valid bitmap file */
+  if (fgetc(fp)!='B' || fgetc(fp)!='M')
+  {
+    fclose(fp);
+    printf("%s is not a bitmap file.\n",file);
+    exit(1);
+  }
+
+  /* read in the width and height of the image, and the
+     number of colors used; ignore the rest */
+  fskip(fp,16);
+  fread(&b->width, sizeof(word), 1, fp);
+  fskip(fp,2);
+  fread(&b->height,sizeof(word), 1, fp);
+  fskip(fp,22);
+  fread(&num_colors,sizeof(word), 1, fp);
+  fskip(fp,6);
+
+  /* assume we are working with an 8-bit file */
+  if (num_colors==0) num_colors=256;
+
+
+  /* try to allocate memory */
+  if ((b->data = (byte *) malloc((word)(b->width*b->height))) == NULL)
+  {
+    fclose(fp);
+    printf("Error allocating memory for file %s.\n",file);
+    exit(1);
+  }
+
+  /* Ignore the palette information for now.
+     See palette.c for code to read the palette info. */
+  fskip(fp,num_colors*4);
+
+  /* read the bitmap */
+  for(index=(b->height-1)*b->width;index>=0;index-=b->width)
+    for(x=0;x<b->width;x++)
+      b->data[(word)index+x]=(byte)fgetc(fp);
+
+  fclose(fp);
+}
+
+/**************************************************************************
+ *  draw_bitmap                                                           *
+ *    Draws a bitmap.                                                     *
+ **************************************************************************/
+
+void draw_bitmap(BITMAP *bmp,int x,int y)
+{
+  int j;
+  word screen_offset = (y<<8)+(y<<6)+x;
+  word bitmap_offset = 0;
+
+  for(j=0;j<bmp->height;j++)
+  {
+    memcpy(&buffer[screen_offset],&bmp->data[bitmap_offset],bmp->width);
+
+    bitmap_offset+=bmp->width;
+    screen_offset+=SCREEN_WIDTH;
+  }
+}
+
+
+
+
+
+/**************************************************************************
+ *  draw_transparent_bitmap                                               *
+ *    Draws a transparent bitmap.                                         *
+ **************************************************************************/
+int kgetpixel(BITMAP *bmp,int x,int y){
+int col;
+
+col=bmp->data[x+(y*320)];
+
+return col;
+
+}
+void kputpixel(char *buffer,int x,int y,int col){
+
+
+buffer[x+(y*320)]=col;
+
+}
+
+
+void kdrawtransbitmap(BITMAP *bmp,char* buffer,int posx,int posy,int w,int h,int colc){
+int x,y;
+int col;
+for (x=posx;x<posx+w;x++){
+for (y=posy;y<posy+h;y++){
+col=kgetpixel(bmp,x,y);
+if (col)  kputpixel(buffer,x,y,col+colc);
+
+}
+}
+
+
+
+
+
+}
+void draw_transparent_bitmap(BITMAP *bmp,int x,int y)
+{
+  int i,j;
+  word screen_offset = (y<<6)+(y<<8);
+  word bitmap_offset = 0;
+  byte data;
+
+  for(j=0;j<bmp->height;j++)
+  {
+    for(i=0;i<bmp->width;i++,bitmap_offset++)
+    {
+      data = bmp->data[bitmap_offset];
+      if (data) buffer[screen_offset+x+i] = data;
+    }
+    screen_offset+=SCREEN_WIDTH;
+  }
+}
+
+/**************************************************************************
+ *  wait                                                                  *
+ *    Wait for a specified number of clock ticks (18hz).                  *
+ **************************************************************************/
+
+void wait(int ticks)
+{
+  word start;
+
+  start=*my_clock;
+
+  while (*my_clock-start<ticks)
+  {
+    *my_clock=*my_clock;              /* this line is for some compilers
+					 that would otherwise ignore this
+					 loop */
+  }
+}
+
+/**************************************************************************
+ *  Main                                                                  *
+ *    Draws opaque and transparent bitmaps                                *
+ **************************************************************************/
+
+void main()
+{
+  int k=1;
+  int i,x,y;
+  int col;
+  BITMAP bmp;
+  BITMAP bmp2;
+  buffer=(char*)malloc(64000U);
+  load_bmp("rocket.bmp",&bmp2);        /* open the file */
+  load_bmp("map.bmp",&bmp);
+  set_mode(VGA_256_COLOR_MODE);       /* set the video mode. */
+
+
+
+     memset(buffer,0,64000);
+
+      draw_transparent_bitmap(&bmp,0,0);
+	   draw_transparent_bitmap(&bmp2,0,100);
+
+col=0;
+while (k==1){
+col++;
+kdrawtransbitmap(&bmp,buffer,220,25,70,45,col);
+kputpixel(buffer,150,10,col);
+  /* draw the background */
+   memcpy(VGA,buffer,64000);
+
+if (kbhit())
+
+{k=0;  }
+
+
+}
+
+//  free(bmp.data);                     /* free up memory used */
+
+  set_mode(TEXT_MODE);                /* set the video mode back to
+					 text mode. */
+
+  return;
+
+
+}
+
